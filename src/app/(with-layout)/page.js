2da891ab9e23f2f3ee51b76364655fce3f534319
reshad21 <miakala7@gmail.com>
2024-01-05 12:48:38 +0600

    bg img add

--- src/app/(with-layout)/page.js
+++ src/app/(with-layout)/page.js
@@ -1,8 +1,30 @@
-
+import heroBg from "@/assets/bannerbg.png";
+import heroImage from "@/assets/heroimage.svg";
+import Image from "next/image";
+import Link from "next/link";
 export default function Home() {
+  console.log(heroBg);
   return (
-    <main className="py-4 max-w-7xl mx-auto min-h-screen">
-      Home page all content
+    <main style={{
+      backgroundImage: `url(${heroBg.src})`,
+      backgroundSize: "cover", // Optional: Adjust as per your needs
+      backgroundPosition: "center", // Optional: Adjust as per your needs
+      width: '100%',
+      height: '100%',
+    }}>
+      <div className="relative mx-auto max-w-7xl p-6 lg:px-8 z-2">
+        <div className="flex justify-between items-center">
+          <div className="">
+            <h1 className="text-4xl lg:text-6xl font-extrabold mb-4 capitalize">You Speak And We Help You Execute It In Your Own Way!! Welcome To WebDev!</h1>
+            <p className="text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque doloremque molestiae maiores dolor ducimus quos distinctio magnam praesentium sunt amet.</p>
+            <div className="flex gap-2">
+              <Link href="/" className="bg-gradient-to-r from-[#ee0979] from-10% via-[#ff6a00] via-70% to-[#ee0979] to-90% shadow-lg px-8 py-3 text-white rounded-xl font-semibold">Get Started</Link>
+              <Link href="/" className="bg-gradient-to-r from-[#ee0979] from-10% via-[#ff6a00] via-70% to-[#ee0979] to-90% shadow-lg px-8 py-3 text-white rounded-xl font-semibold">Know More</Link>
+            </div>
+          </div>
+          <Image src={heroImage} alt="banner image" width={725} height={819} />
+        </div>
+      </div>
     </main>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,27 @@
 import { navData } from "@/data/navber";
+import Link from "next/link";
 import NavLink from "./NavLink";
 
 const Header = () => {
     return (
-        <div className="flex justify-between items-center px-16 py-5 shadow-xl border-collapse ">
-            <h1>Logo</h1>
-            <ul className="flex">
-                {
-                    navData.map(({ path, title }) => (
-                        <li key={path}>
-                            <NavLink href={path} exact={path == "/"} activeClassName="text-orange-500" className="px-3 text-lg">
-                                {title}
-                            </NavLink>
-                        </li>
-                    ))
-                }
-            </ul>
-            <p>Social bar</p>
+        <div className="sticky top-0 z-50 bg-[#4039D4]">
+            <div className="max-w-7xl mx-auto flex items-center justify-between p-6 lg:px-8">
+                <h1>Logo</h1>
+                <ul className="flex">
+                    {
+                        navData.map(({ path, title }) => (
+                            <li key={path} className="text-white">
+                                <NavLink href={path} exact={path == "/"} activeClassName="text-orange-500" className="px-3 text-sm">
+                                    {title}
+                                </NavLink>
+                            </li>
+                        ))
+                    }
+                </ul>
+                <p>
+                    <Link href="/" class="bg-gradient-to-r from-[#ee0979] from-10% via-[#ff6a00] via-70% to-[#ee0979] to-90% shadow-lg px-8 py-3 text-white rounded-xl font-semibold">Contact Us</Link>
+                </p>
+            </div>
         </div>
     );
 };

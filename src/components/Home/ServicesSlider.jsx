ccc02d67d9e93d1f649c4580a13d8e1284b32ed3
reshad21 <miakala7@gmail.com>
2024-01-06 23:50:38 +0600

    shadow given to the slider

--- src/components/Home/ServicesSlider.jsx
+++ src/components/Home/ServicesSlider.jsx
@@ -22,6 +22,11 @@ const imageStyle = {
     objectFit: "cover",
 };
 
+const sliderStyle = {
+    boxShadow: "0px 16px 20px 0px #00000030",
+    borderRadius: "2%"
+}
+
 const ServicesSlider = () => {
     return (
         <div className="w-[50%]">
@@ -32,41 +37,42 @@ const ServicesSlider = () => {
                 }}
                 effect={'fade'}
                 modules={[Autoplay, EffectFade, Pagination, Navigation]}
+                style={sliderStyle}
             >
                 <SwiperSlide>
-                    <div className="relative w-full before:absolute before:h-full before:w-full before:bg-green-100 before:z-[-1] before:rounded-xl">
+                    <div className="relative w-full before:absolute before:h-full before:w-full before:bg-green-100 before:z-[-1]">
                         <Image src={digitalmarketing} style={imageStyle} alt="service images" />
-                        <h2 className="text-center bg-white text-slate-800 font-bold text-xl py-4 border border-slate-200 rounded-lg shadow-2xl">Digital Marketing</h2>
+                        <h2 className="text-center bg-white text-slate-800 font-bold text-xl py-4 border border-slate-200 rounded-lg">Digital Marketing</h2>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className="relative w-full before:absolute before:h-full before:w-full before:bg-blue-100 before:z-[-1] before:rounded-xl">
+                    <div className="relative w-full before:absolute before:h-full before:w-full before:bg-blue-100 before:z-[-1]">
                         <Image src={websiteoptimization} style={imageStyle} alt="service images" />
-                        <h2 className="text-center bg-white text-slate-800 font-bold text-xl py-4 border border-slate-200 rounded-lg shadow-2xl">Website Optimization</h2>
+                        <h2 className="text-center bg-white text-slate-800 font-bold text-xl py-4 border border-slate-200 rounded-lg">Website Optimization</h2>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className="relative w-full before:absolute before:h-full before:w-full before:bg-pink-100 before:z-[-1] before:rounded-xl">
+                    <div className="relative w-full before:absolute before:h-full before:w-full before:bg-pink-100 before:z-[-1]">
                         <Image src={websitedevelopment} style={imageStyle} alt="service images" />
-                        <h2 className="text-center bg-white text-slate-800 font-bold text-xl py-4 border border-slate-200 rounded-lg shadow-2xl">Website Development</h2>
+                        <h2 className="text-center bg-white text-slate-800 font-bold text-xl py-4 border border-slate-200 rounded-lg">Website Development</h2>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className="relative w-full before:absolute before:h-full before:w-full before:bg-yellow-100 before:z-[-1] before:rounded-xl">
+                    <div className="relative w-full before:absolute before:h-full before:w-full before:bg-yellow-100 before:z-[-1]">
                         <Image src={smoservices} style={imageStyle} alt="service images" />
-                        <h2 className="text-center bg-white text-slate-800 font-bold text-xl py-4 border border-slate-200 rounded-lg shadow-2xl">SMO Services</h2>
+                        <h2 className="text-center bg-white text-slate-800 font-bold text-xl py-4 border border-slate-200 rounded-lg">SMO Services</h2>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className="relative w-full before:absolute before:h-full before:w-full before:bg-rose-100 before:z-[-1] before:rounded-xl">
+                    <div className="relative w-full before:absolute before:h-full before:w-full before:bg-rose-100 before:z-[-1]">
                         <Image src={seoservices} style={imageStyle} alt="service images" />
-                        <h2 className="text-center bg-white text-slate-800 font-bold text-xl py-4 border border-slate-200 rounded-lg shadow-2xl">SEO Services</h2>
+                        <h2 className="text-center bg-white text-slate-800 font-bold text-xl py-4 border border-slate-200 rounded-lg">SEO Services</h2>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className="relative w-full before:absolute before:h-full before:w-full before:bg-slate-100 before:z-[-1] before:rounded-xl">
+                    <div className="relative w-full before:absolute before:h-full before:w-full before:bg-slate-100 before:z-[-1]">
                         <Image src={ppcservices} style={imageStyle} alt="service images" />
-                        <h2 className="text-center bg-white text-slate-800 font-bold text-xl py-4 border border-slate-200 rounded-lg shadow-2xl">PPC Services</h2>
+                        <h2 className="text-center bg-white text-slate-800 font-bold text-xl py-4 border border-slate-200 rounded-lg">PPC Services</h2>
                     </div>
                 </SwiperSlide>
             </Swiper>

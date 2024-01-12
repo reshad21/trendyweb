e511aa8f83fe521fe5948f1220edb42ec28a7b52
reshad21 <miakala7@gmail.com>
2024-01-12 09:43:10 +0600

    portfolio component add

--- src/app/(with-layout)/portfolio/page.js
+++ src/app/(with-layout)/portfolio/page.js
@@ -1,8 +1,27 @@
+import PortfolioCard from "@/components/Portfolio/PortfolioCard";
 
 const PortfolioPage = () => {
     return (
-        <div>
-            this is portfolio page
+        <div className="max-w-7xl mx-auto p-6 lg:px-8">
+            <p className="text-center text-3xl font-semibold mb-5">We Have Done Many Others Work <br /> Let’s See Our Works</p>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                <PortfolioCard/>
+                <PortfolioCard/>
+                <PortfolioCard/>
+                <PortfolioCard/>
+                <PortfolioCard/>
+                <PortfolioCard/>
+                <PortfolioCard/>
+                <PortfolioCard/>
+                <PortfolioCard/>
+                <PortfolioCard/>
+                <PortfolioCard/>
+                <PortfolioCard/>
+                <PortfolioCard/>
+                <PortfolioCard/>
+                <PortfolioCard/>
+                <PortfolioCard/>
+            </div>
         </div>
     );
 };

eb7a34524009fe5f5de1c882148e44b0b1b4ad1c
reshad21 <miakala7@gmail.com>
2024-02-17 10:53:23 +0600

    footer alignment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
     return (
-        <footer className=" bg-neutral text-neutral-content w-full">
+        <footer className="bg-neutral text-neutral-content w-full">
             <div className="max-w-7xl mx-auto p-6 lg:px-8 footer">
                 <nav>
                     <header className="footer-title">Services</header>

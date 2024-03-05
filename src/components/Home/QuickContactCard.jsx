3e88d54e6ab18814c84324048a01a74fe6a219ba
reshad21 <miakala7@gmail.com>
2024-03-05 11:52:42 +0600

    wuick contact card create

--- src/components/Home/QuickContactCard.jsx
+++ src/components/Home/QuickContactCard.jsx
@@ -1,11 +1,25 @@
-
+import { FiPhoneCall } from "react-icons/fi";
+import { TfiLocationPin } from "react-icons/tfi";
+import { TiMessages } from "react-icons/ti";
 const QuickContactCard = () => {
     return (
-        <div>
+        <div className="flex justify-between gap-4">
             <div className="card w-full bg-base-100 shadow-xl">
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-body items-center">
+                    <span className=""><FiPhoneCall size={42} className="text-blue-800" /></span>
+                    <p className="text-lg">+880 1787170612</p>
+                </div>
+            </div>
+            <div className="card w-full bg-base-100 shadow-xl">
+                <div className="card-body items-center">
+                    <span className=""><TiMessages size={42} className="text-blue-800" /></span>
+                    <p className="text-lg">info@WebDev.com</p>
+                </div>
+            </div>
+            <div className="card w-full bg-base-100 shadow-xl">
+                <div className="card-body items-center">
+                    <span className=""><TfiLocationPin size={42} className="text-blue-800" /></span>
+                    <p className="text-lg">Indo-Bangla Road, Barishal, 8200, Bangladesh</p>
                 </div>
             </div>
         </div>
